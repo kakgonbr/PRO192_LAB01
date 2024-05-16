d019d9d9d939dcbdb5a1bad76837285bec995113
kakgonbr <155745376+kakgonbr@users.noreply.github.com>
2024-05-16 10:02:26 +0700

    Cleanup + utilize Util

--- lab01/Lab01.java
+++ lab01/Lab01.java
@@ -2,36 +2,23 @@ package lab01;
 
 // JDK 17.0.10
 
+import Misc.Utils;
+
+
 public class Lab01 {
     
     public static void main(String[] args) {
-        java.util.Scanner scanner;
+        int choice;
+        String inp;
 
-        outer:
-        while (true){
-            do{
-                scanner = new java.util.Scanner(System.in);
-                System.out.print("*".repeat(8) + " Menu " + "*".repeat(8) + "\n"
+        do{
+            
+            while ((inp = Misc.Utils.getLine("*".repeat(8) + " Menu " + "*".repeat(8) + "\n"
                                 + "1. Solve quadratic\n"
                                 + "2. Calculate monthly electric bill\n"
                                 + "3. Exit\n"
-                                + "Enter choice: "); 
-            } while (!scanner.hasNextInt());
+                                + "Enter choice: ", Utils.validations.vInt)).isBlank() || (choice = Integer.parseInt(inp)) < 1);
 
-            int choice = scanner.nextInt();
-
-            // System.out.println(choice);
-
-            // JDK 12+
-//            switch(choice){
-//            case 1 -> validateQuadratic();
-//            case 2 -> validateElectricBill();
-//            case 3 -> {
-//                System.out.println("Exiting");
-//                break outer;
-//                }
-//            default -> System.out.println("INVALID INPUT");
-//            } 
             
             switch(choice){
             case 1:
@@ -41,14 +28,11 @@ public class Lab01 {
                 ElectricUsage.validateElectricBill();
                 break;
             case 3:
-                System.out.println("Exiting");
-                break outer;
+                break;
             default:
                 System.out.println("INVALID INPUT");
             }
-        }   
-
-        scanner.close();
+        } while (choice != 3); 
     }
     
 }
